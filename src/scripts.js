12b1fe321fac24901efde954ff2b936c69d17b05
Jelena Jovicic <jejovicic@gmail.com>
2024-05-07 18:28:44 +0200

    Style header

--- src/scripts.js
+++ src/scripts.js
@@ -4,6 +4,7 @@ import './styles.css'
 import apiCalls from './apiCalls'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './images/3.Nutritionist.svg'
 import recipeData from './data/recipes'
 import ingredientsData from './data/ingredients'
 // Below are examples of how you can import functions from either the recipes or domUpdates files.
